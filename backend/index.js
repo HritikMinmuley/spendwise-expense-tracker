aadc5ac60f63ac2a5e0492355e2491d8f5ae09e9
Hritik <hminmuley@gmail.com>
2026-06-29 11:06:38 +0530

    Prepare backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,16 @@ import expenseRoute from "./routes/expense.route.js"
 dotenv.config({});
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 //middleware
 app.use(express.json())
 app.use(urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true
-}
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+};
 app.use(cors(corsOptions))
 
 // api's
